5b6b05eb0c800f5db34c6e60fcadbb6f711b7b2f
Colleenrenehan <cerenehan5@gmail.com>
2023-11-09 14:37:18 -0600

    confetti

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -4,15 +4,23 @@ import { CartItem } from "./cart-item";
 import { useNavigate } from "react-router-dom";
 import DeleteIcon from '@mui/icons-material/Delete';
 import IconButton from '@mui/material/IconButton';
-
-
+import ConfettiExplosion from 'react-confetti-explosion';
 
 import "./cart.css";
+
 export const Cart = () => {
   const { cartItems, getTotalCartAmount, checkout, deleteCart } = useContext(ShopContext);
   const totalAmount = getTotalCartAmount();
 
   const navigate = useNavigate();
+  const [isExploding, setIsExploding] = useState(false);
+
+  const explosionColors = [
+    '#FF5733', 
+    '#FF0000', 
+    '#FFD700', 
+    '#B22222', 
+  ];
 
   return (
     <div className="cart">
@@ -21,28 +29,48 @@ export const Cart = () => {
       </div>
       <div className="cart">
         {cartItems.map((cartItem) => {
-              return <CartItem data={cartItem} key={cartItem.id}/>;
+          return <CartItem data={cartItem} key={cartItem.id} />;
         })}
       </div>
 
       {totalAmount > 0 ? (
         <div className="checkout">
           <p> Subtotal: ${totalAmount} </p>
-          <IconButton aria-label="delete"onClick={deleteCart}><DeleteIcon /></IconButton>
+          <IconButton aria-label="delete" onClick={() => {
+            deleteCart();
+            setIsExploding(true);
+            setTimeout(() => {
+              setIsExploding(false);
+            }, 3000);
+          }}>
+            <DeleteIcon />
+          </IconButton>
           <button onClick={() => navigate("/")}> Continue Shopping </button>
           <button
             onClick={() => {
               checkout();
               navigate("/checkout");
             }}
-          >{" "}
-          
-          Checkout{" "}
+          >
+            {" "}
+            Checkout{" "}
           </button>
         </div>
       ) : (
         <h1> Your Shopping Cart is Empty</h1>
       )}
+      
+      {isExploding && (
+        <ConfettiExplosion
+          colors={explosionColors}
+          particleCount={600}
+          width={175}
+          height={180}
+          force={0.9}
+          duration={3000}
+          
+        />
+      )}
     </div>
   );
-};
+}; 
